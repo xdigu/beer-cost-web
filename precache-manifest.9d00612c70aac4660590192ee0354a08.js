9e056968b36fd8a537bf14f4f461d4d8bcd044b1
Rodrigo Jorge <rodrigo.jorge@businessintegrator.com.br>
2020-07-02 23:22:33 -0300

    Updates

---
+++ precache-manifest.9d00612c70aac4660590192ee0354a08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d22c25e0850f57b8e50a1e1b84c2c30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9ed5959c9dd56d40cb5",
+    "url": "/static/css/main.85474de8.chunk.css"
+  },
+  {
+    "revision": "eeb32544f87985c15685",
+    "url": "/static/js/2.3895b88d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3895b88d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9ed5959c9dd56d40cb5",
+    "url": "/static/js/main.abec1e97.chunk.js"
+  },
+  {
+    "revision": "07c53c9f026e30bc2b32",
+    "url": "/static/js/runtime-main.da9ea447.js"
+  },
+  {
+    "revision": "5fbb0151b360789d07162020754a84c4",
+    "url": "/static/media/header-logo.5fbb0151.svg"
+  }
+]);
