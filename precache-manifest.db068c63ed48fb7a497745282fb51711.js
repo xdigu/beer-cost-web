e73127fb3d16641059e4250783a0ab1a1bb5195a
Rodrigo Jorge <rodrigo.jorge@businessintegrator.com.br>
2020-07-02 23:41:12 -0300

    Updates

---
+++ precache-manifest.db068c63ed48fb7a497745282fb51711.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e3aa06a6f564eefa7fe8a8918620c6d",
+    "url": "/beer-cost-web/index.html"
+  },
+  {
+    "revision": "a9ed5959c9dd56d40cb5",
+    "url": "/beer-cost-web/static/css/main.85474de8.chunk.css"
+  },
+  {
+    "revision": "eeb32544f87985c15685",
+    "url": "/beer-cost-web/static/js/2.3895b88d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/beer-cost-web/static/js/2.3895b88d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9ed5959c9dd56d40cb5",
+    "url": "/beer-cost-web/static/js/main.abec1e97.chunk.js"
+  },
+  {
+    "revision": "f3945af396161fe3438f",
+    "url": "/beer-cost-web/static/js/runtime-main.08c8e769.js"
+  },
+  {
+    "revision": "5fbb0151b360789d07162020754a84c4",
+    "url": "/beer-cost-web/static/media/header-logo.5fbb0151.svg"
+  }
+]);
